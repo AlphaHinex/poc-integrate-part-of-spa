39488e4d124ae342aa52b35fe78ae67e2daee180
AlphaHinex <AlphaHinex@gmail.com>
2015-05-19 16:14:42 +0800

    Use class, not id

--- integrate-angularjs.js
+++ integrate-angularjs.js
@@ -1,26 +1,26 @@
 function renderSnippet(ngAppName, urlPrefix, templateUrl) {
-  $('#angularBlock').empty();
-  $('#angularBlock').append('<div id="innerBlock"><ng-view /></div>');
+  $('.angularBlock').empty();
+  $('.angularBlock').append('<div class="innerBlock"><ng-view /></div>');
   if(urlPrefix && templateUrl) {
     $.get(urlPrefix + templateUrl)
       .done(function(data) {
-        $('#innerBlock').append(data);
+        $('.innerBlock').append(data);
       })
       .done(function() {
         $.when(loadNgInclude(urlPrefix))
           .done(function() {
-            angular.bootstrap($('#innerBlock'), [ngAppName]);
+            angular.bootstrap($('.innerBlock'), [ngAppName]);
           });
       });
   } else {
-    angular.bootstrap($('#innerBlock'), [ngAppName]);
+    angular.bootstrap($('.innerBlock'), [ngAppName]);
   }
 }
 
 function loadNgInclude(urlPrefix) {
   var dtd = $.Deferred();
   var requests = Array();
-  var includes = $('#innerBlock').find('[ng-include]');
+  var includes = $('.innerBlock').find('[ng-include]');
   includes.each(function(index, el) {
     var url = $(el).attr('src') || $(el).attr('ng-include')
     url = url.replace(/[\'\"]/g, '');
